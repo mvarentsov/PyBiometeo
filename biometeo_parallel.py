4deda694884bf8eccb6858736a66a93ed90d2b3c
Mikhail Varensov <mvar91@gmail.com>
2025-09-20 00:42:39 +0300

    parallel calculations

---
+++ biometeo_parallel.py
@@ -0,0 +1,63 @@
+import numpy as np
+from multiprocessing import Pool 
+from tqdm import tqdm
+import biometeo
+from functools import partial
+from itertools import product
+import xarray as xr
+
+# Define the wrapper function at the module level
+def _func_wrapper(func, result_key, d):
+    ds = d['ds']
+
+    all_idx = np.atleast_2d(d['idx'])
+
+    if result_key is not None:
+        results = [func(**{key:ds[key][tuple(idx)] for key in ds.keys()})[result_key] for idx in all_idx]
+    else:
+        results = [func(**{key:ds[key][tuple(idx)] for key in ds.keys()}) for idx in all_idx]
+
+    return np.array(results)
+
+def compute_parallel(func, params4chunks, n_jobs, result_key=None, use_tqdm=True):
+    wrapped = partial(_func_wrapper, func, result_key)
+    
+    if n_jobs > 1:
+        with Pool(processes=n_jobs) as pool:
+            if use_tqdm:
+                results = list(tqdm(pool.imap(wrapped, params4chunks), total=len(params4chunks)))
+            else:
+                results = list(pool.imap(wrapped, params4chunks))
+    else:
+        if use_tqdm:
+            results = list(tqdm(map(wrapped, params4chunks), total=len(params4chunks)))
+        else:
+            results = list(map(wrapped, params4chunks))
+    
+    return np.concatenate(results)
+
+
+def compute4xarray_ds (func, ds, params:dict, n_jobs, n_chunks=None, result_key=None, use_tqdm=True, min_chunk_size=2):
+    
+    first_param = ds[list(params.values())[0]]
+
+    shape = first_param.shape
+
+    all_indices = np.array(list(product(*[range(dim) for dim in shape])))
+
+    ds2dict = {func_var_name: ds[param_var_name].values for func_var_name, param_var_name in params.items()}
+
+    if n_chunks is not None and n_chunks < len(all_indices)*min_chunk_size:
+        all_indices_chunked = np.array_split(all_indices, n_chunks)
+    else:
+        all_indices_chunked = all_indices
+    
+    ds2dict = [{'ds': ds2dict, 'idx': idx_chunk} for idx_chunk in all_indices_chunked]
+
+    results = compute_parallel(func, ds2dict, n_jobs, result_key, use_tqdm)
+
+    results = results.reshape(shape)
+
+    results = xr.DataArray(results, coords=first_param.coords, dims=first_param.dims)
+    return results
+    
